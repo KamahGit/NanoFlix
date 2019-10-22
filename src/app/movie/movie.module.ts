fb7fee8f847d2b3b5ae3c8fd4b10f9e48e6ec8d4
KamahGit <kamahgenius@gmail.com>
2019-10-22 18:00:10 +0300

    Day 2-extras

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
-  declarations: [MovieListComponent],
+  declarations: [MovieListComponent, MovieDetailsComponent],
   imports: [
     CommonModule,
     MovieRoutingModule
